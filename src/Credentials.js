d0f5ccd71fd624c3e04b4e095c72f4d506559ac7
Ankita Makade <78643106+Ankita196@users.noreply.github.com>
2021-10-01 22:42:19 +0530

    Create Credentials.js

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '829c86735838435a8477d0cb6ca8f47c',
+        ClientSecret: '5d55ada06e004c74ac7f428cbffb7ccb'
+    }
+}
+
+export { Credentials };
